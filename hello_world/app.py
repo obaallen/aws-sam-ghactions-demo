5f0ac1db8d65620ace957b9b45cc2057f139d1ae
Allen Obayagbona <35696261+obaallen@users.noreply.github.com>
2021-07-21 13:18:29 -0400

    Update app.py

--- hello_world/app.py
+++ hello_world/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello folks!",
+            "message": "hello folks",
             # "location": ip.text.replace("\n", "")
         }),
     }
